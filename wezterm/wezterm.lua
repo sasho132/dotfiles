a497221cd0495db9fc170bde2aa73503a047d6d3
Aleksandar Petrov <sasho1320@gmail.com>
2024-08-08 20:16:43 +0300

    remove wezterm background transparency

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -14,15 +14,15 @@ config.initial_rows = 30
 config.initial_cols = 110
 config.send_composed_key_when_left_alt_is_pressed = true
 config.window_close_confirmation = "NeverPrompt"
-config.background = {
-	{
-		source = {
-			Color = "#1e1e2e",
-		},
-		width = "100%",
-		height = "100%",
-		opacity = 0.80,
-	},
-}
+-- config.background = {
+-- 	{
+-- 		source = {
+-- 			Color = "#1e1e2e",
+-- 		},
+-- 		width = "100%",
+-- 		height = "100%",
+-- 		opacity = 0.80,
+-- 	},
+-- }
 
 return config
