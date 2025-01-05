4af45a1c79b9869ff09132509aefa7e912f7fb1f
Aleksandar Petrov <sasho1320@gmail.com>
2025-01-05 13:58:15 +0200

    update nvim codeium plugin

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -1,15 +1,11 @@
 return {
-  'Exafunction/codeium.vim',
+  'Exafunction/codeium.nvim',
   event = 'BufEnter',
-  config = function()
-    vim.keymap.set('i', '<c-;>', function()
-      return vim.fn['codeium#CycleCompletions'](1)
-    end, { expr = true, silent = true })
-    vim.keymap.set('i', '<c-,>', function()
-      return vim.fn['codeium#CycleCompletions'](-1)
-    end, { expr = true, silent = true })
-    vim.keymap.set('i', '<c-x>', function()
-      return vim.fn['codeium#Clear']()
-    end, { expr = true, silent = true })
-  end,
+  opts = {
+    virtual_text = {
+      filetypes = {
+        markdown = false,
+      },
+    },
+  },
 }
