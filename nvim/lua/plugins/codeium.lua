ccc8844a90c99397e4f997f77f4735356aeb2dd3
Aleksandar Petrov <sasho1320@gmail.com>
2025-01-05 15:15:03 +0200

    nvim update codeium plugin config

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -1,11 +1,32 @@
 return {
   'Exafunction/codeium.nvim',
-  event = 'BufEnter',
-  opts = {
-    virtual_text = {
-      filetypes = {
-        markdown = false,
-      },
-    },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
   },
+  config = function()
+    require('codeium').setup {
+      enable_cmp_source = false,
+      virtual_text = {
+        enabled = true,
+        idle_delay = 75,
+        filetypes = {
+          markdown = false,
+        },
+        key_bindings = {
+          -- Accept the current completion.
+          accept = '<M-\\>',
+          -- Accept the next word.
+          accept_word = false,
+          -- Accept the next line.
+          accept_line = false,
+          -- Clear the virtual text.
+          clear = false,
+          -- Cycle to the next completion.
+          next = '<M-]>',
+          -- Cycle to the previous completion.
+          prev = '<M-[>',
+        },
+      },
+    }
+  end,
 }
